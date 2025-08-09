8cdff6cea2cfec980c9adf545273975942510ddf
Mohit Soni <developer_mohitsoni@outlook.com>
2025-08-09 21:53:26 +0530

    Update package.json to add dotenv dependency and reorder existing dependencies

--- auth-api/auth-app.js
+++ auth-api/auth-app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require("dotenv").config();
+
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
